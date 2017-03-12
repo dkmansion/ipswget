26d3663fe90f07a5cf11e3bee8c0de3055c10071
Donald M <dkmansion@users.noreply.github.com>
2017-03-11 21:20:13 -0800

    Update ipswget.sh

--- ipswget.sh
+++ ipswget.sh
@@ -6,12 +6,23 @@
 # Example ./ipswget.sh 10.2.1 <enter>
 
 version=$1
+filelocation="/Users/Shared/acFirmware"
 
-curl http://ax.phobos.apple.com.edgesuite.net/WebObjects/MZStore.woa/wa/com.apple.jingle.appserver.client.MZITunesClientCheck/version | grep ipsw | grep $version | sort -u | sed 's/<string>//g' | sed 's/<\/string>//g' | grep -v protected | awk '{$1=$1}1' > /tmp/ipswlist_iOS${version}.txt
+curl http://ax.phobos.apple.com.edgesuite.net/WebObjects/MZStore.woa/wa/com.apple.jingle.appserver.client.MZITunesClientCheck/version | grep ipsw | grep $version | sort -u | sed 's/<string>//g' | sed 's/<\/string>//g' | grep -v protected | awk '{$1=$1}1' >> /tmp/ipswList_iOS${version}.txt
 
-sort -k 1.89 -o /Users/Shared/acFirmware/iOS${version}_All.txt /tmp/ipswlist_iOS${version}.txt
+sort -k 1.89 -o ${filelocation}/iOS${version}_All.txt /tmp/ipswlist_iOS${version}.txt
 
-cat /tmp/ipswlist_iOS${version}.txt | grep iPhone > /Users/Shared/acFirmware/ipswListiOS${version}_iPhone.txt
-cat /tmp/ipswlist_iOS${version}.txt | grep iPad > /Users/Shared/acFirmware/ipswListiOS${version}_iPad.txt
-cat /tmp/ipswlist_iOS${version}.txt | grep iPodtouch > /Users/Shared/acFirmware/ipswListiOS${version}_iPodtouch.txt
-rm -rf /tmp/ipswlist_iOS${version}.txt
+cat /tmp/ipswList_iOS${version}.txt | grep iPhone > ${filelocation}/ipswListiOS${version}_iPhone.txt
+cat /tmp/ipswList_iOS${version}.txt | grep iPad > ${filelocation}/ipswListiOS${version}_iPad.txt
+cat /tmp/ipswList_iOS${version}.txt | grep iPodtouch > ${filelocation}/ipswListiOS${version}_iPodtouch.txt
+rm -rf /tmp/ipswList_iOS${version}.txt
+
+##	Uncomment lines to auto open the list(s) for viewing.
+#open ${filelocation}/ipswListiOS${version}_iPhone.txt
+#open ${filelocation}/ipswListiOS${version}_iPad.txt
+#open ${filelocation}/ipswListiOS${version}_iPodtouch.txt
+
+## Uncomment lines to automatically loop through the list and download all files.
+#for i in $(cat ${filelocation}/ipswListiOS${version}_iPhone.txt) ; do curl -O $i ; done
+#for i in $(cat ${filelocation}/ipswListiOS${version}_iPad.txt) ; do curl -O $i ; done
+#for i in $(cat ${filelocation}/ipswListiOS${version}_iPodtouch.txt) ; do curl -O $i ; done
