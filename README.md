e5e809074552b8bde23276bda8787c68facd4dfb
Donald Mansion <dkmansion@gmail.com>
2017-07-17 16:49:23 -0700

    refactor
    Refactor and add vargs to control version, device files to download,
    and path to store them in.

--- README.md
+++ README.md
@@ -3,26 +3,62 @@ Lookup ipsw restore file links by device and iOS version and download all versio
 
 Setup:
   chmod +x ipswget.sh
-  Move ipswlist.sh to PATH or your firmware storage folder.
-  
+  move ipswget.sh to your PATH or your firmware storage folder.
+
   When I am using Apple Configurator 2, I manage firmware in the following location since AC2 *should* be able to see it cached there.
-  
+
   ~/Library/Group\ Containers/K36BKF7T3D.group.com.apple.configurator/Library/Caches/Firmware/
-  
-  I also symlink it to an easier filesystem location for regular use. 
+
+  I also symlink it to an easier filesystem location for regular use.
     ie:   ls -s ~/Library/Group\ Containers/K36BKF7T3D.group.com.apple.configurator/Library/Caches/Firmware/ /Users/Shared/acFirmware/
-    
+
 USAGE:
-  If script is in your PATH
-    ipswget.sh "{ipws version number}"
-    ie.   ipswget.sh 10.2.1
-  
-  or if running from your firmware folder location
-    ./ipswget.sh "{ipsw versionNumber}"
-    ie.   ./ipswget.sh 10.2.1
-    
-Next Steps:
-  Uncomment "open" or "for" lines to automatically view or download the actual ipsw files.
-  
+	Run ipswget without and inputs to view the usage info.
+	      ____ ____  _____ _       __          ______ ______ ______
+	     /  _// __ \/ ___/| |     / / ../..   / ____// ____//_  __/
+	     / / / /_/ /\__ \ | | /| / / (    (  / / __ / __/    / /
+	   _/ / / ____/___/ / | |/ |/ /   \_._/ / /_/ // /___   / /
+	  /___//_/    /____/  |__/|__/          \____//_____/  /_/
+
+	ipswget version 1.0.0 2017-07-10 17:19:35 PDT
+	(c) dkmansion 2016
+	Purpose:        Query Apple for current IPSW (iOS Restore) file list based
+	                on supplied version number, and optionally download the files.
+
+	Usage:
+	        [options][arguments]
+
+	Options:
+	    -v  Apple iOS version number to retreive (argument as tuple ie. 1.2.3)
+	    -d  Devices to download the ipsw files for as OCTAL sums. (see arguments)
+	    -p  Path for working downloads.
+
+	Arguments:
+	  for option
+	    v   tuple representing iOS version number 10.1.2 or 9.1, etc.
+	    d   Summed OCTAL mimicing chmod permission numbering.
+	      0  -- DOES NOT download any files except the list
+	      1  -- gets iPad ipsws
+	      2  -- gets iPhone ipsws
+	      3  -- gets iPad & iPhone ipsws
+	      4  -- gets iPod ipsws
+	      5  -- gets iPad & iPod ipsws
+	      6  -- gets iPhone & iPod ipsws
+	      7  -- gets All (iPad, then iPhone, then iPod) ipsws
+
+	    p   Valid file path where you want to download the files.
+	        	ie ~/downloads
+
+	Examples:
+	        Get the list for 10.3.1, and DOES NOT DOWNLOAD ANY ipsw files (-d = 0)
+	        ipswget -d 0 -v 10.3.1 -p ~/Downloads
+
+	        Get the list for 10.3.2 and downloads all iPad ipsws to ~/downloads (-d = 1)
+	        ipswget -d 1 -v 10.3.2 -p ~/downloads
+
+	        Get the list for 10.3.2 and downloads all iPad ipsws to current directory (missing -p)
+	        ipswget -d 1 -v 10.3.2
+
+|- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
 NOTE: ipsws downloaded are ~2.5GB each.
-    
--- ipswget.sh
+++ ipswget.sh
@@ -1,28 +1,185 @@
 #!/bin/bash
-# Get list of ipsw files from Apple download for version specified in variable parameter
-# usage ./ipswget.sh "{versionNumber}"
-# Remember to chmod +x ipswget.sh
-# You can place it in your PATH or your firmware folder to 
-# Example ./ipswget.sh 10.2.1 <enter>
+#Get list of ipsw files from Apple download for version specified in variable parameter
+while getopts "v:d:p:" option
+do
+	case "${option}"
+		in
+			v) version="${OPTARG}";;
+			d) devices=${OPTARG};;
+			p) path="${OPTARG}";;
+	esac
+done
 
-version=$1
-filelocation="/Users/Shared/acFirmware"
+clear
 
-curl http://ax.phobos.apple.com.edgesuite.net/WebObjects/MZStore.woa/wa/com.apple.jingle.appserver.client.MZITunesClientCheck/version | grep ipsw | grep $version | sort -u | sed 's/<string>//g' | sed 's/<\/string>//g' | grep -v protected | awk '{$1=$1}1' >> /tmp/ipswList_iOS${version}.txt
+black=`tput setaf 0`
+red=`tput setaf 1`
+green=`tput setaf 2`
+gold=`tput setaf 3`
+blue=`tput setaf 4`
+magenta=`tput setaf 5`
+cyan=`tput setaf 6`
+white=`tput setaf 7`
+grey=`tput setaf 8`
+yellow=`tput setaf 11`
+reset=`tput sgr0`
 
-sort -k 1.89 -o ${filelocation}/iOS${version}_All.txt /tmp/ipswlist_iOS${version}.txt
+#echo "${red}red text ${green}green text${reset}"
 
-cat /tmp/ipswList_iOS${version}.txt | grep iPhone > ${filelocation}/ipswListiOS${version}_iPhone.txt
-cat /tmp/ipswList_iOS${version}.txt | grep iPad > ${filelocation}/ipswListiOS${version}_iPad.txt
-cat /tmp/ipswList_iOS${version}.txt | grep iPodtouch > ${filelocation}/ipswListiOS${version}_iPodtouch.txt
-rm -rf /tmp/ipswList_iOS${version}.txt
+if [ "${version}" == "" ] && [ "${devices}" == "" ] && [ "${path}" == "" ]; then
+	echo "
+      ${gold}____ ____  _____ _       __          ______ ______ ______
+     /  _// __ \/ ___/| |     / / ../..   / ____// ____//_  __/
+     / / / /_/ /\__ \ | | /| / / (    (  / / __ / __/    / /
+   _/ / / ____/___/ / | |/ |/ /   \_._/ / /_/ // /___   / /
+  /___//_/    /____/  |__/|__/          \____//_____/  /_/${reset}
 
-##	Uncomment lines to auto open the list(s) for viewing.
-#open ${filelocation}/ipswListiOS${version}_iPhone.txt
-#open ${filelocation}/ipswListiOS${version}_iPad.txt
-#open ${filelocation}/ipswListiOS${version}_iPodtouch.txt
+${gold}ipswget${reset} version ${gold}1.0.0${reset} 2017-07-10 17:19:35 PDT
+(c) dkmansion 2016
+${gold}Purpose:${reset}	Query Apple for current IPSW (iOS Restore) file list based
+		on supplied version number, and optionally download the files.
 
-## Uncomment lines to automatically loop through the list and download all files.
-#for i in $(cat ${filelocation}/ipswListiOS${version}_iPhone.txt) ; do curl -O $i ; done
-#for i in $(cat ${filelocation}/ipswListiOS${version}_iPad.txt) ; do curl -O $i ; done
-#for i in $(cat ${filelocation}/ipswListiOS${version}_iPodtouch.txt) ; do curl -O $i ; done
+${gold}Usage:${reset}
+	[options][arguments]
+
+${gold}Options:${reset}
+    ${green}-v${reset}	Apple iOS version number to retreive (argument as tuple ie. 1.2.3)
+    ${green}-d${reset}	Devices to download the ipsw files for as OCTAL sums. (see arguments)
+    ${green}-p${reset}	Path for working downloads.
+
+${gold}Arguments:${reset}
+  for option
+    ${green}v${reset}	tuple representing iOS version number ${magenta}10.1.2${reset} or ${magenta}9.1${reset}, etc.
+    ${green}d${reset}	Summed OCTAL mimicing chmod permission numbering.
+      ${magenta}0${reset}	 -- DOES NOT download any files except the list
+      ${magenta}1${reset}	 -- gets iPad ipsws
+      ${magenta}2${reset}	 -- gets iPhone ipsws
+      ${magenta}3${reset}	 -- gets iPad & iPhone ipsws
+      ${magenta}4${reset}	 -- gets iPod ipsws
+      ${magenta}5${reset}	 -- gets iPad & iPod ipsws
+      ${magenta}6${reset}	 -- gets iPhone & iPod ipsws
+      ${magenta}7${reset}	 -- gets All (iPad, then iPhone, then iPod) ipsws
+
+	${green}p${reset}	Valid file path where you want to download the files.
+			ie ~/downloads
+
+Examples:
+	Get the list for 10.3.1, and DOES NOT DOWNLOAD ANY ipsw files (-d = 0)
+	${gold}ipswget ${green}-d ${magenta}0 ${green}-v ${magenta}10.3.1 ${green}-p ${magenta}~/Downloads${reset}
+
+	Get the list for 10.3.2 and downloads all iPad ipsws to ~/downloads (-d = 1)
+	${gold}ipswget ${green}-d ${magenta}1 ${green}-v ${magenta}10.3.2 ${green}-p ${magenta}~/downloads${reset}
+
+	Get the list for 10.3.2 and downloads all iPad ipsws to current directory (missing -p)
+	${gold}ipswget ${green}-d ${magenta}1 ${green}-v ${magenta}10.3.2 ${reset}
+
+"
+fi
+
+echo "Version : ${version}"
+echo "Devices : " && case "${devices}"
+	in
+	0) echo -e "\t${red}DOES NOT download any ipsw files ONLY the list.";;
+	1) echo -e "\t${green}Will download iPad ipsw files.";;
+	2) echo -e "\t${green}Will download iPhone ipsw files.";;
+	3) echo -e "\t${green}Will download iPad & iPhone ipsw files.";;
+	4) echo -e "\t${green}Will download iPod ipsw files.";;
+	5) echo -e "\t${green}Will download iPad & iPod ipsw files.";;
+	6) echo -e "\t${green}Will download iPhone & iPod ipsw files.";;
+	7) echo -e "\t${green}Will download All (iPad, then iPhone, then iPod) ipsw files.${reset}";;
+esac
+
+if [ "${version}" == "" ]; then
+	echo "-v Version is required."
+else
+	if [ "${devices}" == "" ]; then
+		echo "-d Devices is needed, though 0 will be assumed if You must include a single OCTAL value with option -d{OCTAL} to tell the script which files types to download.
+		ie. # ./listandgetipsws.sh -v{ios_version} -d{OCTAL}
+		eg. # ./listandgetipsws.sh -v10.3.2 -d7  --Gets all device ipsw files for version 10.3.2 available file list.
+
+		Add digits together to get multiple device files.
+			0 -- DOES NOT download any files except the list
+			1 -- gets iPad
+			2 -- gets iPhone
+			3 -- gets iPad & iPhone
+			4 -- gets iPod
+			5 -- gets iPad & iPod
+			6 -- gets iPhone & iPod
+			7 -- gets All (iPad, then iPhone, then iPod)
+
+		!!! Generally we will use 1, 2, 3 or 0.";
+
+	else
+		ac2firmwarecache="~/Library/Group\ Containers/K36BKF7T3D.group.com.apple.configurator/Library/Caches/Firmware"
+		mypath="/Users/Shared/acFirmware"
+
+		if [ "${path}" == "" ]; then
+			echo -e "No destination Path provided with -p option. \n\nChecking for ${mypath} ..."
+			#This path is used for my iOS device management processes.  Feel free to change it to your default.  See the reasoning.readme for this folder use in my organization.
+			if [[ -d "${mypath}" ]]; then
+				echo "Found '${mypath}'"
+				echo "Using '${mypath}' for file downloads"
+				path="${mypath}"
+			elif [[ -d "'${ac2firmwarecache}'" ]]; then
+				echo -e "${mypath} not found.\n
+				Found ${ac2firmwarecache}"
+				path="${ac2firmwarecache}"
+			else
+				path=$(pwd);
+				echo "${mypath} not found. ${ac2firmwarecache} not found."
+				echo "Using current directory ${path} for file downloads"
+
+			fi
+		fi
+
+
+		echo -e "\tDownloading the list of urls for the ipsw restore files for iOS version ${version}"
+		##exit
+		curl http://ax.phobos.apple.com.edgesuite.net/WebObjects/MZStore.woa/wa/com.apple.jingle.appserver.client.MZITunesClientCheck/version | grep ipsw | grep $version | sort -u | sed 's/<string>//g' | sed 's/<\/string>//g' | grep -v protected | awk '{$1=$1}1' > /tmp/ipswlist_iOS${version}.txt
+
+		# sort the URLs at character 89 to get all device types together.
+		sort -k 1.89 -o ${path}/iOS${version}_All.txt /tmp/ipswlist_iOS${version}.txt;
+
+		# If $path/lists does not exist make directory
+
+		if [ ! -d "${path}/ipswlists" ]; then
+			mkdir -p "${path}/ipswlists"
+		fi
+
+		# Create individual url lists for each device type.
+
+		cat /tmp/ipswlist_iOS${version}.txt | grep iPad > ${path}/ipswlists/ipswListiOS${version}_iPad.txt;
+		#@TODO::dkmansion - calculate total lines and estimate total file size for downloads chosen.
+		cat /tmp/ipswlist_iOS${version}.txt | grep iPhone > ${path}/ipswlists/ipswListiOS${version}_iPhone.txt;
+
+		cat /tmp/ipswlist_iOS${version}.txt | grep iPodtouch > ${path}/ipswlists/ipswListiOS${version}_iPodtouch.txt;
+
+
+		files=$(echo `wc -l < /tmp/ipswlist_iOS${version}.txt`);
+		filesizeestimate=$(($files*2));
+
+		echo $files;
+		echo $filesizeestimate;
+
+		exit;
+
+		# destroy the temp file
+		rm -rf /tmp/ipswlist_iOS${version}.txt;
+
+		# Change directory to $path to curl the ipsw files into.
+		cd ${path};
+
+		# if option -d OCTAL sums to 1 (iPad), 3
+		if [ ${devices} == 1 ] || [ ${devices} == 3 ] || [ ${devices} == 5 ] || [ ${devices} == 7 ]; then
+			for i in $(cat ${path}/ipswlists/ipswListiOS${version}_iPhone.txt) ; do curl -O $i; done
+		fi
+
+		if [ ${devices} == 2 ] || [ ${devices} == 3 ] || [ ${devices} == 6 ] || [ ${devices} == 7 ]; then
+			for i in $(cat ${path}/ipswlists/ipswListiOS${version}_iPad.txt) ; do curl -O $i; done
+		fi
+
+		if [ ${devices} == 4 ] || [ ${devices} == 5 ] || [ ${devices} == 6 ] || [ ${devices} == 7 ]; then
+			for i in $(cat ${path}/ipswlists/ipswListiOS${version}_iPodtouch.txt) ; do curl -O $i; done
+		fi
+	fi
+fi
